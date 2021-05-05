98f94290edfbee43ec1d688a94278d9dad427bd8
MikeMathews123 <juggernaut9434@gmail.com>
2021-05-05 07:58:09 -0700

    user logic building password and uuid and id are static and therefore wrong

--- src/backend/database.js
+++ src/backend/database.js
@@ -9,14 +9,45 @@ const path = require("path");
 
 
 const databaseFile = path.join(__dirname, "../data/database.json");
-const adapter = new FileSync<DbSchema>(databaseFile);
+const adapter = new FileSync(databaseFile);
 const db = low(adapter);
 
 // seed the database
+export const seedDb = () => {
+
+    // parse the JSON seed
+    const dbSeed = JSON.parse(
+        fs.readFileSync("../data/database-seed.json")
+    );
+
+    // write the seed into the current database
+    db.setState(dbSeed).write();
+    return;
+};
 
 // create user
+export const createUser = (userDetails) => {
+
+    // encrypt the password in the db
+    const password = null; // TODO , fill
+    
+    // make user object.
+    const user = {
+        id: 0,
+        uuid: null,
+        username,
+        password,
+        createdAt: new Date(),
+        modifiedAt: new Date()
+    };
+
+    saveUser(user);
+    return user;
+};
 
 // save user to database
+
+
 
 // get user
 
